3e1b834f149a0d17eabe444a0afd5b0ba696abb3
Thanos Patapis <atpatapis@gmail.com>
2019-01-21 00:23:03 +0200

    Ditch State Manager in favor of Subscriber Map and Service

--- src/main/java/di/smartliving/server/config/SmartLivingServerConfig.java
+++ src/main/java/di/smartliving/server/config/SmartLivingServerConfig.java
@@ -1,29 +1,30 @@
 package di.smartliving.server.config;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import di.smartliving.server.domain.Profile;
-import di.smartliving.server.domain.repository.ProfileRepository;
-import di.smartliving.server.global.StateManager;
+import di.smartliving.server.domain.StorageUnit;
+import di.smartliving.server.domain.repository.StorageUnitRepository;
 import di.smartliving.server.properties.MqttProperties;
+import di.smartliving.server.web.mqtt.client.MqttSubscriber;
 
 @Configuration
 @EnableConfigurationProperties(value = MqttProperties.class)
-@EntityScan(basePackageClasses = Profile.class)
-@EnableJpaRepositories(basePackageClasses = ProfileRepository.class)
+@EntityScan(basePackageClasses = StorageUnit.class)
+@EnableJpaRepositories(basePackageClasses = StorageUnitRepository.class)
 @EnableJpaAuditing
-public class ServerAppConfig {
+public class SmartLivingServerConfig {
 
 	@Bean
-	@Scope("singleton")
-	public StateManager stateManager() {
-		return new StateManager();
+	public ConcurrentHashMap<String, Set<MqttSubscriber>> subscriberMap() {
+		return new ConcurrentHashMap<>();
 	}
-	
+
 }
